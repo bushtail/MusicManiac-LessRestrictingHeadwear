cff7fb5f841122c64209f6b6d823d1ab4c769654
MusicManiac <14209235+MusicManiac@users.noreply.github.com>
2023-06-15 06:36:57 +0200

    Add files via upload

---
+++ MusicManiac-LessRestrictingHelmets/src/mod.js
@@ -0,0 +1,31 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class LessRestrictingHelmets {
+    constructor() {
+        this.config = require("../config/config.json");
+    }
+    postDBLoad(container) {
+        // Get the logger from the server container.
+        const logger = container.resolve("WinstonLogger");
+        // Get database from server.
+        const db = container.resolve("DatabaseServer");
+        // Get tables from database
+        const tables = db.getTables();
+        // Get item database from tables
+        const itemDB = tables.templates.items;
+        let helmetesAdjusted = 0, faceSheildsAdjusted = 0;
+        for (let item in itemDB) {
+            // check if item has helmet parent
+            if (itemDB[item]._parent == "5a341c4086f77401f2541505") {
+                if (this.config.allowHeadPhonesOnAllHelmets && itemDB[item]._props.BlocksEarpiece) {
+                    itemDB[item]._props.BlocksEarpiece = false;
+                    logger.info(itemDB[item]._props.Name + " - " + itemDB[item]._props.BlocksEarpiece);
+                    helmetesAdjusted++;
+                }
+            }
+        }
+        logger.info("MusicManiac - Less Restricting Helmets Loaded:");
+        logger.info(helmetesAdjusted + " helmets modified to allow headphones");
+    }
+}
+module.exports = { mod: new LessRestrictingHelmets() };
---
+++ MusicManiac-LessRestrictingHelmets/src/mod.ts
@@ -0,0 +1,40 @@
+import { DependencyContainer } from "tsyringe";
+import { Ilogger } from "@spt-aki/models/spt/utils/Ilogger";
+import { IPostDBLoadMod } from "@spt-aki/models/external/IPostDBLoadMod";
+import { DatabaseServer } from "@spt-aki/servers/DatabaseServer";
+
+class LessRestrictingHelmets implements IPostDBLoadMod
+{
+	private config = require("../config/config.json");
+
+	public postDBLoad(container: DependencyContainer): void 
+	{
+		// Get the logger from the server container.
+		const logger = container.resolve<Ilogger>("WinstonLogger");
+		// Get database from server.
+		const db = container.resolve<DatabaseServer>("DatabaseServer");
+		// Get tables from database
+		const tables = db.getTables();    
+		// Get item database from tables
+		const itemDB = tables.templates.items;
+
+		let helmetesAdjusted = 0, faceSheildsAdjusted = 0
+		
+		for (let item in itemDB) {
+			// check if item has helmet parent
+			if (itemDB[item]._parent == "5a341c4086f77401f2541505") {
+				if (this.config.allowHeadPhonesOnAllHelmets && itemDB[item]._props.BlocksEarpiece) {
+					itemDB[item]._props.BlocksEarpiece = false;
+					logger.info(itemDB[item]._props.Name + " - " + itemDB[item]._props.BlocksEarpiece);
+					helmetesAdjusted++;
+				}
+			}
+		}
+
+			logger.info("MusicManiac - Less Restricting Helmets Loaded:");
+			logger.info(helmetesAdjusted + " helmets modified to allow headphones");
+		}
+	}
+}
+
+module.exports = { mod: new LessRestrictingHelmets() }
