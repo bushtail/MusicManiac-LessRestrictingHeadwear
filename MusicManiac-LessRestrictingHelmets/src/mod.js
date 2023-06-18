94c3082a9a235c24320f8331e6dc354e0ad8bd17
MusicManiac <14209235+MusicManiac@users.noreply.github.com>
2023-06-18 13:49:37 +0200

    v.1.1.0
    Added faceshields

--- MusicManiac-LessRestrictingHelmets/src/mod.js
+++ MusicManiac-LessRestrictingHelmets/src/mod.js
@@ -13,19 +13,24 @@ class LessRestrictingHelmets {
         const tables = db.getTables();
         // Get item database from tables
         const itemDB = tables.templates.items;
-        let helmetesAdjusted = 0, faceSheildsAdjusted = 0;
+        let helmetesAdjusted = 0, facemasksAdjusted = 0;
         for (let item in itemDB) {
             // check if item has helmet parent
-            if (itemDB[item]._parent == "5a341c4086f77401f2541505") {
-                if (this.config.allowHeadPhonesOnAllHelmets && itemDB[item]._props.BlocksEarpiece) {
-                    itemDB[item]._props.BlocksEarpiece = false;
-                    //logger.info(itemDB[item]._props.Name + " - " + itemDB[item]._props.BlocksEarpiece);
-                    helmetesAdjusted++;
-                }
+            if (this.config.allowHeadPhonesOnAllHelmets && itemDB[item]._parent == "5a341c4086f77401f2541505" && itemDB[item]._props.BlocksEarpiece) {
+                itemDB[item]._props.BlocksEarpiece = false;
+                //logger.info(itemDB[item]._props.Name + " - " + itemDB[item]._props.BlocksEarpiece);
+                helmetesAdjusted++;
+            }
+            // // check if item has facemask parent
+            if (this.config.allowHeadPhonesOnAllFacemasks && itemDB[item]._parent == "5a341c4686f77469e155819e" && itemDB[item]._props.BlocksEarpiece) {
+                itemDB[item]._props.BlocksEarpiece = false;
+                //logger.info(itemDB[item]._props.Name + " - " + itemDB[item]._props.BlocksEarpiece);
+                facemasksAdjusted++;
             }
         }
         logger.info("MusicManiac - Less Restricting Helmets Loaded:");
         logger.info(helmetesAdjusted + " helmets modified to allow headphones");
+        logger.info(facemasksAdjusted + " facemasks modified to allow headphones");
     }
 }
 module.exports = { mod: new LessRestrictingHelmets() };
