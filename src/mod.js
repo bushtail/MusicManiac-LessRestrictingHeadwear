42aeaffa6b8b139a76021c8d32e4dd1773f4dd5d
MusicManiac <14209235+MusicManiac@users.noreply.github.com>
2023-06-21 19:13:47 +0200

    v.1.3.0
    Added option for helmets that can be used with some headsets

--- src/mod.js
+++ src/mod.js
@@ -13,12 +13,20 @@ class LessRestrictingHedwear {
         const tables = db.getTables();
         // Get item database from tables
         const itemDB = tables.templates.items;
-        let allowAllHelmetsWithAllHeadsets = 0, allowAllFacemasksWithAllHeadsets = 0, allowAllFacemasksWithAllHelmets = 0;
+        let allowAllHelmetsWithAllHeadsets = 0, allowAllFacemasksWithAllHeadsets = 0, allowAllFacemasksWithAllHelmets = 0, removeAllHelmetAndHeadsetRestrictions = 0;
+        // 5a341c4086f77401f2541505 - headwear
+        // 5a341c4686f77469e155819e - facecover
+        // 5645bcb74bdc2ded0b8b4578 - headphones
         for (let item in itemDB) {
             // allowAllHelmetsWithAllHeadsets - find all helmets and unblock earpiece
             if (this.config.allowAllHelmetsWithAllHeadsets && itemDB[item]._parent == "5a341c4086f77401f2541505" && itemDB[item]._props.BlocksEarpiece) {
                 itemDB[item]._props.BlocksEarpiece = false;
                 allowAllHelmetsWithAllHeadsets++;
+            }
+            // removeAllHelmetAndHeadsetRestrictions - remove restricted items from helmets/headsets
+            if (this.config.removeAllHelmetAndHeadsetRestrictions && (itemDB[item]._parent == "5a341c4086f77401f2541505" || itemDB[item]._parent == "5645bcb74bdc2ded0b8b4578") && itemDB[item]._props.ConflictingItems.length > 0) {
+                itemDB[item]._props.ConflictingItems.length = 0;
+                removeAllHelmetAndHeadsetRestrictions++;
             }
             // allowAllFacemasksWithAllHeadsets - find all facecovers and unblock earpiece
             if (this.config.allowAllFacemasksWithAllHeadsets && itemDB[item]._parent == "5a341c4686f77469e155819e" && itemDB[item]._props.BlocksEarpiece) {
@@ -36,6 +44,7 @@ class LessRestrictingHedwear {
         logger.info("[Less Restricting Headwear] " + allowAllHelmetsWithAllHeadsets + " helmets modified to allow headphones");
         logger.info("[Less Restricting Headwear] " + allowAllFacemasksWithAllHeadsets + " facemasks modified to allow headphones");
         logger.info("[Less Restricting Headwear] " + allowAllFacemasksWithAllHelmets + " facemasks and helmets modified to allow facemask+helmet combo");
+        logger.info("[Less Restricting Headwear] " + removeAllHelmetAndHeadsetRestrictions + " helmets/headphones conflicting items have been purged");
     }
 }
 module.exports = { mod: new LessRestrictingHedwear() };
