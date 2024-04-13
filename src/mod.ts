c4229e922491e017e2d1d2dc215d88f2f355bc83
MusicManiac <14209235+MusicManiac@users.noreply.github.com>
2024-04-13 03:07:02 +0200

    Added faceshields, rewrote the code

--- src/mod.ts
+++ src/mod.ts
@@ -2,6 +2,8 @@ import { DependencyContainer } from "tsyringe";
 import { Ilogger } from "@spt-aki/models/spt/utils/Ilogger";
 import { IPostDBLoadMod } from "@spt-aki/models/external/IPostDBLoadMod";
 import { DatabaseServer } from "@spt-aki/servers/DatabaseServer";
+import { ItemHelper } from "@spt-aki/helpers/ItemHelper";
+import { BaseClasses } from  "@spt-aki/models/enums/BaseClasses";
 
 class LessRestrictingHedwear implements IPostDBLoadMod
 {
@@ -9,45 +11,63 @@ class LessRestrictingHedwear implements IPostDBLoadMod
 
 	public postDBLoad(container: DependencyContainer): void 
 	{
-		// Get the logger from the server container.
 		const logger = container.resolve<Ilogger>("WinstonLogger");
-		// Get database from server.
 		const db = container.resolve<DatabaseServer>("DatabaseServer");
-		// Get tables from database
 		const tables = db.getTables();    
-		// Get item database from tables
 		const itemDB = tables.templates.items;
+		const itemHelper = container.resolve<ItemHelper>("ItemHelper");
 
-		let allowAllHelmetsWithAllHeadsets = 0, allowAllFacemasksWithAllHeadsets = 0, allowAllFacemasksWithAllHelmets = 0, removeAllHelmetAndHeadsetRestrictions = 0;
+		let allowAllHelmetsWithAllHeadsets = 0, allowAllFacemasksWithAllHeadsets = 0, allowAllFacemasksWithAllHelmets = 0, removeAllHelmetAndHeadsetRestrictions = 0, removeAllFacemasksRestrictions = 0;
 
-		// 5a341c4086f77401f2541505 - headwear
 		// 5a341c4686f77469e155819e - facecover
 		// 5645bcb74bdc2ded0b8b4578 - headphones
 		
 		for (let item in itemDB) {
-			// allowAllHelmetsWithAllHeadsets - find all helmets and unblock earpiece
-			if (this.config.allowAllHelmetsWithAllHeadsets && itemDB[item]._parent == "5a341c4086f77401f2541505" && itemDB[item]._props.BlocksEarpiece){
-				itemDB[item]._props.BlocksEarpiece = false;
-				allowAllHelmetsWithAllHeadsets++;
+			if (itemDB[item]._type !== "Node") {
+				const itemId = itemDB[item]._id
+				if (itemHelper.isOfBaseclass(itemId, BaseClasses.HEADWEAR) && itemDB[item]._props.BlocksEarpiece) {
+					if (this.config.allowAllHelmetsWithAllHeadsets && itemDB[item]._props.BlocksEarpiece) {
+						itemDB[item]._props.BlocksEarpiece = false;
+						allowAllHelmetsWithAllHeadsets++;
+					}
+					if (this.config.removeAllHelmetAndHeadsetRestrictions && itemDB[item]._props.ConflictingItems.length > 0) {
+						itemDB[item]._props.ConflictingItems.length = 0;
+						removeAllHelmetAndHeadsetRestrictions++;
+					}
+					if (this.config.allowAllFacemasksWithAllHelmets && itemDB[item]._props.BlocksFaceCover) {
+						itemDB[item]._props.BlocksFaceCover = false;
+						allowAllFacemasksWithAllHelmets++;
+					}
+				}
+				if (itemHelper.isOfBaseclass(itemId, BaseClasses.HEADPHONES)) {
+					if (this.config.removeAllHelmetAndHeadsetRestrictions && itemDB[item]._props.ConflictingItems.length > 0) {
+						itemDB[item]._props.ConflictingItems.length = 0;
+						removeAllHelmetAndHeadsetRestrictions++;
+					}
+				}
+				if (itemHelper.isOfBaseclass(itemId, BaseClasses.FACECOVER)) {
+					if (this.config.allowAllFacemasksWithAllHeadsets && itemDB[item]._props.BlocksEarpiece) {
+						itemDB[item]._props.ConflictingItems.length = 0;
+						removeAllHelmetAndHeadsetRestrictions++;
+					}
+					if (this.config.allowAllFacemasksWithAllHelmets && itemDB[item]._props.BlocksFaceCover) {
+						itemDB[item]._props.BlocksFaceCover = false;
+						allowAllFacemasksWithAllHelmets++;
+					}
+					if (this.config.removeAllFacemasksRestrictions && itemDB[item]._props.ConflictingItems.length > 0) {
+						itemDB[item]._props.ConflictingItems.length = 0;
+						removeAllFacemasksRestrictions++;
+					}
+				}
+
 			}
-			// removeAllHelmetAndHeadsetRestrictions - remove restricted items from helmets/headsets
-			if (this.config.removeAllHelmetAndHeadsetRestrictions && (itemDB[item]._parent == "5a341c4086f77401f2541505" || itemDB[item]._parent == "5645bcb74bdc2ded0b8b4578") && itemDB[item]._props.ConflictingItems.length > 0) {
-				itemDB[item]._props.ConflictingItems.length = 0;
-				removeAllHelmetAndHeadsetRestrictions++;
+		}
+
+		for (const id of this.config.faceshieldsToCleanConflictingItemsFrom) {
+			if (itemDB[id]._props.ConflictingItems.length > 0) {
+				itemDB[id]._props.ConflictingItems.length = 0;
+				removeAllFacemasksRestrictions++;
 			}
-			// allowAllFacemasksWithAllHeadsets - find all facecovers and unblock earpiece
-			if (this.config.allowAllFacemasksWithAllHeadsets && itemDB[item]._parent == "5a341c4686f77469e155819e" && itemDB[item]._props.BlocksEarpiece) {
-				itemDB[item]._props.BlocksEarpiece = false;
-				allowAllFacemasksWithAllHeadsets++;
-			}
-			// allowAllFacemasksWithAllHelmets - find all helmets and facecovers and unblock facecover/helmet slot
-			if (this.config.allowAllFacemasksWithAllHelmets && (itemDB[item]._parent == "5a341c4686f77469e155819e" || itemDB[item]._parent == "5a341c4086f77401f2541505") && (itemDB[item]._props.BlocksFaceCover || itemDB[item]._props.BlocksHeadwear || itemDB[item]._props.ConflictingItems.length > 0)) {
-				itemDB[item]._props.ConflictingItems.length = 0;
-				itemDB[item]._props.BlocksFaceCover = false;
-				itemDB[item]._props.BlocksHeadwear = false;
-				allowAllFacemasksWithAllHelmets++;
-			}
-			
 		}
 
 		logger.info("[Less Restricting Headwear] MusicManiac - Less Restricting Headwear Loaded:");
@@ -55,6 +75,7 @@ class LessRestrictingHedwear implements IPostDBLoadMod
 		logger.info("[Less Restricting Headwear] " + allowAllFacemasksWithAllHeadsets + " facemasks modified to allow headphones");
 		logger.info("[Less Restricting Headwear] " + allowAllFacemasksWithAllHelmets + " facemasks and helmets modified to allow facemask+helmet combo");
 		logger.info("[Less Restricting Headwear] " + removeAllHelmetAndHeadsetRestrictions + " helmets/headphones conflicting items have been purged");
+		logger.info("[Less Restricting Headwear] " + removeAllFacemasksRestrictions + " facemasks conflicting items have been purged");
 	}
 }
 
