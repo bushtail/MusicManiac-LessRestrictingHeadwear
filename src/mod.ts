ebf5fbdbe878288f582cfe105b66160e7584bb1d
bushtail <evannosich@icloud.com>
2025-03-24 15:29:02 -0400

    Update mod.ts

--- src/mod.ts
+++ src/mod.ts
@@ -1,18 +1,22 @@
 import { DependencyContainer } from "tsyringe";
-import { Ilogger } from "@spt/models/spt/utils/Ilogger";
+import { ILogger } from "@spt/models/spt/utils/Ilogger";
 import { IPostDBLoadMod } from "@spt/models/external/IPostDBLoadMod";
 import { DatabaseServer } from "@spt/servers/DatabaseServer";
 import { ItemHelper } from "@spt/helpers/ItemHelper";
 import { BaseClasses } from "@spt/models/enums/BaseClasses";
 
-import { VFS } from "@spt/utils/VFS";
+import { existsSync } from "node:fs";
+import { readFileSync } from "node:fs";
 import { jsonc } from "jsonc";
 import path from "path";
 
 class LessRestrictingHeadwear implements IPostDBLoadMod
 {
 	public mod: string;
-    public modShortName: string;
+    	public modShortName: string;
+	private logger: ILogger;
+	private config: any;
+	
 
 	constructor() {
         this.mod = "MusicManiac - Less Restricting Headwear";
@@ -21,67 +25,81 @@ class LessRestrictingHeadwear implements IPostDBLoadMod
 
 	public postDBLoad(container: DependencyContainer): void 
 	{
-		const logger = container.resolve<Ilogger>("WinstonLogger");
+		this.logger = container.resolve<ILogger>("WinstonLogger");
 		const db = container.resolve<DatabaseServer>("DatabaseServer");
 		const tables = db.getTables();    
 		const itemDB = tables.templates.items;
 		const itemHelper = container.resolve<ItemHelper>("ItemHelper");
-
-		const vfs = container.resolve<VFS>("VFS");
-		const config = jsonc.parse(vfs.readFile(path.resolve(__dirname, "../config.jsonc")));
 		
+		const configPath = path.resolve(__dirname, "../config.jsonc");
+		if(!existsSync(configPath)) {
+			this.logger.error(`${this.modShortName} Config file not found at ${configPath}`);
+			return;
+		}
+		
+		try
+		{
+			this.config = jsonc.parse(readFileSync(configPath, "utf8"));
+			if(!this.config) {
+				this.logger.error(`${this.mod} Failed to parse config file or config is empty`);
+				return;
+			}
+		} catch (e) {
+			this.logger.error(`${this.mod} Error parsing config file: ${e instanceof Error ? e.message : String(e)}`);
+			return;
+		}
 		for (let item in itemDB) {
 			if (itemDB[item]._type !== "Node") {
 				const itemId = itemDB[item]._id
 				if (itemHelper.isOfBaseclass(itemId, BaseClasses.HEADWEAR)) {
-					if (config.debug) {
-						logger.info(`[${this.modShortName}] adjusting item ${itemDB[item]._name} (id ${itemId} ) to match config values`);
+					if (this.config.debug) {
+						this.logger.info(`[${this.modShortName}] adjusting item ${itemDB[item]._name} (id ${itemId} ) to match config values`);
 					}
-					itemDB[item]._props.BlocksHeadwear = config.Headwear.removeBlocksHeadwear ? false : itemDB[item]._props.BlocksHeadwear;
-					itemDB[item]._props.BlocksEarpiece = config.Headwear.removeBlocksEarpiece ? false : itemDB[item]._props.BlocksEarpiece;
-					itemDB[item]._props.BlocksFaceCover = config.Headwear.removeBlocksFaceCover ? false : itemDB[item]._props.BlocksFaceCover;
-					itemDB[item]._props.BlocksEyewear = config.Headwear.removeBlocksEyewear ? false : itemDB[item]._props.BlocksEyewear;
-					itemDB[item]._props.ConflictingItems = config.Headwear.clearConflictingItems ? [] : itemDB[item]._props.ConflictingItems;
+					itemDB[item]._props.BlocksHeadwear = this.config.Headwear.removeBlocksHeadwear ? false : itemDB[item]._props.BlocksHeadwear;
+					itemDB[item]._props.BlocksEarpiece = this.config.Headwear.removeBlocksEarpiece ? false : itemDB[item]._props.BlocksEarpiece;
+					itemDB[item]._props.BlocksFaceCover = this.config.Headwear.removeBlocksFaceCover ? false : itemDB[item]._props.BlocksFaceCover;
+					itemDB[item]._props.BlocksEyewear = this.config.Headwear.removeBlocksEyewear ? false : itemDB[item]._props.BlocksEyewear;
+					itemDB[item]._props.ConflictingItems = this.config.Headwear.clearConflictingItems ? [] : itemDB[item]._props.ConflictingItems;
 				} else if (itemHelper.isOfBaseclass(itemId, BaseClasses.HEADPHONES)) {
-					if (config.debug) {
-						logger.info(`[${this.modShortName}] adjusting item ${itemDB[item]._name} (id ${itemId} ) to match config values`);
+					if (this.config.debug) {
+						this.logger.info(`[${this.modShortName}] adjusting item ${itemDB[item]._name} (id ${itemId} ) to match config values`);
 					}
-					itemDB[item]._props.BlocksHeadwear = config.Earpiece.removeBlocksHeadwear ? false : itemDB[item]._props.BlocksHeadwear;
-					itemDB[item]._props.BlocksEarpiece = config.Earpiece.removeBlocksEarpiece ? false : itemDB[item]._props.BlocksEarpiece;
-					itemDB[item]._props.BlocksFaceCover = config.Earpiece.removeBlocksFaceCover ? false : itemDB[item]._props.BlocksFaceCover;
-					itemDB[item]._props.BlocksEyewear = config.Earpiece.removeBlocksEyewear ? false : itemDB[item]._props.BlocksEyewear;
-					itemDB[item]._props.ConflictingItems = config.Earpiece.clearConflictingItems ? [] : itemDB[item]._props.ConflictingItems;
+					itemDB[item]._props.BlocksHeadwear = this.config.Earpiece.removeBlocksHeadwear ? false : itemDB[item]._props.BlocksHeadwear;
+					itemDB[item]._props.BlocksEarpiece = this.config.Earpiece.removeBlocksEarpiece ? false : itemDB[item]._props.BlocksEarpiece;
+					itemDB[item]._props.BlocksFaceCover = this.config.Earpiece.removeBlocksFaceCover ? false : itemDB[item]._props.BlocksFaceCover;
+					itemDB[item]._props.BlocksEyewear = this.config.Earpiece.removeBlocksEyewear ? false : itemDB[item]._props.BlocksEyewear;
+					itemDB[item]._props.ConflictingItems = this.config.Earpiece.clearConflictingItems ? [] : itemDB[item]._props.ConflictingItems;
 				} else if (itemHelper.isOfBaseclass(itemId, BaseClasses.FACECOVER)) {
-					if (config.debug) {
-						logger.info(`[${this.modShortName}] adjusting item ${itemDB[item]._name} (id ${itemId} ) to match config values`);
+					if (this.config.debug) {
+						this.logger.info(`[${this.modShortName}] adjusting item ${itemDB[item]._name} (id ${itemId} ) to match config values`);
 					}
-					itemDB[item]._props.BlocksHeadwear = config.FaceCover.removeBlocksHeadwear ? false : itemDB[item]._props.BlocksHeadwear;
-					itemDB[item]._props.BlocksEarpiece = config.FaceCover.removeBlocksEarpiece ? false : itemDB[item]._props.BlocksEarpiece;
-					itemDB[item]._props.BlocksFaceCover = config.FaceCover.removeBlocksFaceCover ? false : itemDB[item]._props.BlocksFaceCover;
-					itemDB[item]._props.BlocksEyewear = config.FaceCover.removeBlocksEyewear ? false : itemDB[item]._props.BlocksEyewear;
-					itemDB[item]._props.ConflictingItems = config.FaceCover.clearConflictingItems ? [] : itemDB[item]._props.ConflictingItems;
+					itemDB[item]._props.BlocksHeadwear = this.config.FaceCover.removeBlocksHeadwear ? false : itemDB[item]._props.BlocksHeadwear;
+					itemDB[item]._props.BlocksEarpiece = this.config.FaceCover.removeBlocksEarpiece ? false : itemDB[item]._props.BlocksEarpiece;
+					itemDB[item]._props.BlocksFaceCover = this.config.FaceCover.removeBlocksFaceCover ? false : itemDB[item]._props.BlocksFaceCover;
+					itemDB[item]._props.BlocksEyewear = this.config.FaceCover.removeBlocksEyewear ? false : itemDB[item]._props.BlocksEyewear;
+					itemDB[item]._props.ConflictingItems = this.config.FaceCover.clearConflictingItems ? [] : itemDB[item]._props.ConflictingItems;
 				} else if (itemHelper.isOfBaseclass(itemId, BaseClasses.VISORS)) {
-					if (config.debug) {
-						logger.info(`[${this.modShortName}] adjusting item ${itemDB[item]._name} (id ${itemId} ) to match config values`);
+					if (this.config.debug) {
+						this.logger.info(`[${this.modShortName}] adjusting item ${itemDB[item]._name} (id ${itemId} ) to match config values`);
 					}
-					itemDB[item]._props.BlocksHeadwear = config.Eyewear.removeBlocksHeadwear ? false : itemDB[item]._props.BlocksHeadwear;
-					itemDB[item]._props.BlocksEarpiece = config.Eyewear.removeBlocksEarpiece ? false : itemDB[item]._props.BlocksEarpiece;
-					itemDB[item]._props.BlocksFaceCover = config.Eyewear.removeBlocksFaceCover ? false : itemDB[item]._props.BlocksFaceCover;
-					itemDB[item]._props.BlocksEyewear = config.Eyewear.removeBlocksEyewear ? false : itemDB[item]._props.BlocksEyewear;
-					itemDB[item]._props.ConflictingItems = config.Eyewear.clearConflictingItems ? [] : itemDB[item]._props.ConflictingItems;
-				} else if (config.faceShields.includes(itemId)) {
-					if (config.debug) {
-						logger.info(`[${this.modShortName}] adjusting item ${itemDB[item]._name} (id ${itemId} ) to match config values`);
+					itemDB[item]._props.BlocksHeadwear = this.config.Eyewear.removeBlocksHeadwear ? false : itemDB[item]._props.BlocksHeadwear;
+					itemDB[item]._props.BlocksEarpiece = this.config.Eyewear.removeBlocksEarpiece ? false : itemDB[item]._props.BlocksEarpiece;
+					itemDB[item]._props.BlocksFaceCover = this.config.Eyewear.removeBlocksFaceCover ? false : itemDB[item]._props.BlocksFaceCover;
+					itemDB[item]._props.BlocksEyewear = this.config.Eyewear.removeBlocksEyewear ? false : itemDB[item]._props.BlocksEyewear;
+					itemDB[item]._props.ConflictingItems = this.config.Eyewear.clearConflictingItems ? [] : itemDB[item]._props.ConflictingItems;
+				} else if (this.config.faceShields.includes(itemId)) {
+					if (this.config.debug) {
+						this.logger.info(`[${this.modShortName}] adjusting item ${itemDB[item]._name} (id ${itemId} ) to match config values`);
 					}
-					itemDB[item]._props.BlocksHeadwear = config.FaceShields.removeBlocksHeadwear ? false : itemDB[item]._props.BlocksHeadwear;
-					itemDB[item]._props.BlocksEarpiece = config.FaceShields.removeBlocksEarpiece ? false : itemDB[item]._props.BlocksEarpiece;
-					itemDB[item]._props.BlocksFaceCover = config.FaceShields.removeBlocksFaceCover ? false : itemDB[item]._props.BlocksFaceCover;
-					itemDB[item]._props.BlocksEyewear = config.FaceShields.removeBlocksEyewear ? false : itemDB[item]._props.BlocksEyewear;
-					itemDB[item]._props.ConflictingItems = config.FaceShields.clearConflictingItems ? [] : itemDB[item]._props.ConflictingItems;
+					itemDB[item]._props.BlocksHeadwear = this.config.FaceShields.removeBlocksHeadwear ? false : itemDB[item]._props.BlocksHeadwear;
+					itemDB[item]._props.BlocksEarpiece = this.config.FaceShields.removeBlocksEarpiece ? false : itemDB[item]._props.BlocksEarpiece;
+					itemDB[item]._props.BlocksFaceCover = this.config.FaceShields.removeBlocksFaceCover ? false : itemDB[item]._props.BlocksFaceCover;
+					itemDB[item]._props.BlocksEyewear = this.config.FaceShields.removeBlocksEyewear ? false : itemDB[item]._props.BlocksEyewear;
+					itemDB[item]._props.ConflictingItems = this.config.FaceShields.clearConflictingItems ? [] : itemDB[item]._props.ConflictingItems;
 				}
 			}
 		}
-		logger.info(`[${this.modShortName}] ${this.mod} Loaded`);
+		this.logger.info(`[${this.modShortName}] ${this.mod} Loaded`);
 	}
 }
 
